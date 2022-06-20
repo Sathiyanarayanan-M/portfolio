6bb07e05b6db68471f5a75556d171a7146b4ec44
Sathiyanarayanan <sathiyastar2000@gmail.com>
2022-06-20 08:54:59 +0530

    update server.js

--- packages/server/server.js
+++ packages/server/server.js
@@ -8,6 +8,10 @@ const errorHandler = require("./app/middleware/errorHandler");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
