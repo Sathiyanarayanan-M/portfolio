b6fe7d82d2eec191434746eeb1f5623717d890be
Sathiyanarayanan <sathiyastar2000@gmail.com>
2022-06-20 09:20:17 +0530

    change index.js in configurations

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   target: "node",
-  entry: "./app.js",
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
