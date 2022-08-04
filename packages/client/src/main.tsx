a3e7d2e18189044847aac245604626b0f87ae42b
Sathiyanarayanan <sathiyastar2000@gmail.com>
2022-08-04 09:52:41 +0530

    comment strict mode

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import * as App from "src/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App.Main />
-  </React.StrictMode>
+  //   <React.StrictMode>
+  <App.Main />
+  //   </React.StrictMode>
 );
