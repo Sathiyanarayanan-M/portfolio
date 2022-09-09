5170a682884455b3c73054ac7a4331e36a7ad380
Sathiyanarayanan <sathiyastar2000@gmail.com>
2022-09-09 09:53:52 +0530

    add fab component on blog

---
+++ packages/client/src/app/pages/blog/views/FloatingActionButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import * as Router from "react-router-dom";
+import * as Mui from "@mui/material";
+import * as MuiIcons from "@mui/icons-material";
+import * as Constants from "src/constants";
+import styles from "src/app/pages/blog/views/styles.module.scss";
+
+export const FloatingActionButton = () => {
+  const navigate = Router.useNavigate();
+  return (
+    <div className={styles.blog__fab__wrapper}>
+      <Mui.IconButton
+        size="large"
+        disableRipple
+        className={styles.fab}
+        onClick={() => navigate("/blog/post-blog")}
+      >
+        <MuiIcons.Create />
+      </Mui.IconButton>
+    </div>
+  );
+};
