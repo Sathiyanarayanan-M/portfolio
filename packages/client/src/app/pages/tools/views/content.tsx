24d93cf8c7058fec89c698400c5c9a4335bf54d5
Sathiyanarayanan <sathiyastar2000@gmail.com>
2022-10-29 20:09:58 +0530

    udpates

--- packages/client/src/app/pages/tools/views/content.tsx
+++ packages/client/src/app/pages/tools/views/content.tsx
@@ -3,7 +3,7 @@ import * as Pages from "src/app/pages";
 
 export const Content = () => {
   return (
-    <Mui.Box sx={{ p: 2 }}>
+    <Mui.Box sx={{ p: { md: 5, xs: 3 } }}>
       <Mui.Grid container>
         {Pages.Tools.ToolsList.map((item, idx) => (
           <Mui.Grid item xs={12} sm={6} md={4} key={idx}>
