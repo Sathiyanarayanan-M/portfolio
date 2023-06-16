57f5057f18ebb82870e374de73a3f801a8ae29a3
Sathiyanarayanan-M <sathiyastar2000@gmail.com>
2023-06-16 22:15:33 +0530

    feat(server): use keys directly instead of env

--- packages/server/app/config/firebaseConfig.js
+++ packages/server/app/config/firebaseConfig.js
@@ -1,12 +1,41 @@
 exports.FIREBASE_CONFIG = {
-  type: process.env.type,
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key.replace(/\\n/g, '\n'),
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
+  type: "service_account",
+  project_id: "sathiyanarayanan-m",
+  private_key_id: "d57cba142fd8e6ec57c1d265ba73ffa09901ac8d",
+  private_key: `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC9Ed2vubBBn9KZ
+bKtAf5efKpBWDT1PbmbZLh7xJmmS/Zir1v4PLfn63rocXRG17/qlsNWqktoLwRVg
+f0VpO5qlrNl1g1BnQtvhU4YjvuqRFBzVhzN2v32PeU0Q3+5BZunUiJelfkoJi915
+dksXP64JY31oIg1HpixeVjpJk3Bx2r10kqPi4IY0Ca15w+yQY2N7RdejrtjuBA+5
+b5Ek7gfyDODvugA/ByTRpVcBMh2iV+jdAn5SUzyXoODSeoPrCmHABau7Mmx/vGEF
+WhrGvEIXm3D0rIkhrCTMtyyA9gMr+WJOxNF4Px6xILoGhg1kZ2abeYGi5c+btxor
+Yh9SuF0BAgMBAAECggEABkkgRPphLjegOouxG9Av0uQJJojwkssZdzsSXrNQuRI5
+r6RANHJFyFlFZbIk+fJB2C+dnk0vZLiAqVzUHNlwpfVANvIjcDyMSq7u9xH6JxtO
+2vPizHlZ/jUGPjtN5tEn2js1uAG0LtjsBzw4nd4t46tNSqnCAWiTzS47HyHJ65Jz
+33mF3WKc+4Md0frKcciD0lqdcjhKBPCqvWijUyWovvozad8qc8m81pK9Zh5cZhdV
+laLwvDqi7MYQx7Xi0zM+s/4rU1AHmXZ+uUZzd67NuZZtV1xiNnLPG6KRVhhRUvoh
+gRTxMs+saGSyDQqbFpZQzwn2eyocmTiqhE8DQcW2KwKBgQD25aKEombH/lwt4+Je
+CI9C5QAlpb2Gwx+xV5T6jNaiuchO0EHayi4uCAma1HvxQJMIm9L8ZgBD52//IyyY
+6ZEGV7gWRfsfYXbveloxmvTic8T21lAp6B8sUSraO65Y9RleMlSg1FcPwX7o5xSZ
+8Ct61wUKk0a9HB+6M+llwYMHswKBgQDECmwiKp+vTnMSZyvoUIJe8VkUncCdnYwB
+wJ7wjf0SaRxZcowB06lp1KCH0B5w8mNvERwX+uIy03mBRYM/uBbUqUS+GNjcbnOd
+VD4SVFePqrxGdjanwboKbr7bT7PHmq9zPw5B1yzcR11hsySo/BJMI99M/Zu57PDy
+S4DdzFiuewKBgQDJm0vw+FfrWVXWFy6CUIUDP9G17Dft8ilP+89oJFcju4bIbKBJ
+0yyQY/nL11R59JvbvLo1ZekzianXcdZ56seEoV5tualMv56KAKPju7/ZpmUglnIR
+Nj02R/UxDOjaNRII7BpU1e9dq0hBUGhvJ0Gp5ih0sTxjMbLqrncYo0KmpwKBgAzz
+B0uGdz/UNcOsbvkMgGAXHyQ+e1+t7XacK6OGLYZOzSGXcGoaeiFUezodZDVfvXOz
+tKBc4R4sW+VPGylVVqS7fZ/08jY1UYu96c+6JIKH66HX4ypXoJtZz1+bNyvjKt24
+948f0IXIbQGsHKrQ48CFyhVC10uDvDK1DBEak20LAoGAVakg7ZX0y7FlDForP19u
+ly9BMnDk2ZODpVra/RPeGfc94IfubapfBCEQo5I9fwLz6EopfNbxJJrlXMbGLL/l
+6qXAxidIImC+ZXOxTzm8oGUhgg//OXA5AoWSoV0TIGx5LqWBJwaPAn7vqRpPc59+
+YV/Qe3IRszHQzI3KB9bdn9k=
+-----END PRIVATE KEY-----`,
+  client_email:
+    "firebase-adminsdk-knlvf@sathiyanarayanan-m.iam.gserviceaccount.com",
+  client_id: "101841458521292364906",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-knlvf%40sathiyanarayanan-m.iam.gserviceaccount.com",
 };
