da24abb8890750988e5d28eddfa8289eb0e3a50e
Sathiyanarayanan <61767157+Sathiyanarayanan-M@users.noreply.github.com>
2023-07-23 14:34:46 +0000

    update ignore build

--- packages/server/vercel-ignore-build.sh
+++ packages/server/vercel-ignore-build.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
+#!/bin/bash
 
 echo "VERCEL_ENV: $VERCEL_ENV"
 
-# Check if the current branch is "production"
-if git diff --quiet --exit-code -- packages/server ; then
-  # Check if there are changes in the "packages/server" folder
+if [[ "$VERCEL_ENV" == "production" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
   exit 1;
-  # if [[ "$VERCEL_ENV" == "production" ]] ; then
-  #   # No changes in "packages/server" folder, so cancel the build
-  #   echo "🛑 - Build cancelled for non-production environment."
-  #   exit 0;
-  # else
-  #   # Proceed with the build since there are changes in "packages/server" folder
-  #   echo "✅ - Build can proceed"
-  #   exit 1;
-  fi
+
 else
-  # Don't build for non-production environments
-  echo "🛑 - No changes in 'packages/server' folder. Build cancelled."
+  # Don't build
+  echo "🛑 - Build cancelled"
   exit 0;
 fi
